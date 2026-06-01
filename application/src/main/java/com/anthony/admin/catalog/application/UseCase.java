7471aa0a1c701b0f116d921062d24117208008fb
Anthony Lopez <tony.lopez.15@gmail.com>
2026-06-01 15:23:59 -0500

    refactor domain exceptions

--- application/src/main/java/com/anthony/admin/catalog/application/UseCase.java
+++ application/src/main/java/com/anthony/admin/catalog/application/UseCase.java
@@ -5,6 +5,6 @@ import com.anthony.admin.catalog.domain.category.Category;
 public class UseCase {
 
     public Category execute(){
-//        return new Category();
+        return Category.newCategory("", "", true);
     }
 }
